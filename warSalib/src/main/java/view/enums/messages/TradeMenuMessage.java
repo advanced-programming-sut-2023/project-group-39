ef9ce0f5dbad21660538fc769921c8bd2578213f
Keyhan-Hodaei <118798460+Keyhan-Hodaei@users.noreply.github.com>
2023-04-23 12:02:07 +0430

    TradeMenuMessage: completed messages

--- warSalib/src/main/java/view/enums/messages/TradeMenuMessage.java
+++ warSalib/src/main/java/view/enums/messages/TradeMenuMessage.java
@@ -1,4 +1,13 @@
 package view.enums.messages;
 
 public enum TradeMenuMessage {
+    INVALID_AMOUNT_OR_PRICE,
+    ITEM_NOT_EXIST,
+    EMPTY_TRADE_LIST,
+    ID_NOT_EXIST,
+    INSUFFICIENT_FUNDING,
+    EMPTY_TRADE_HISTORY,
+    SUCCESS,
+
+
 }
