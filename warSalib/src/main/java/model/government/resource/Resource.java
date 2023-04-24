d046dd6aaa23441304ef0b4d7e5a8e5a5260eb56
sepehr <ghiassepehr@gmail.com>
2023-04-25 01:49:39 +0330

    building : change resources

--- warSalib/src/main/java/model/government/resource/Resource.java
+++ warSalib/src/main/java/model/government/resource/Resource.java
@@ -1,6 +1,19 @@
 package model.government.resource;
 
 public enum Resource {
-    STONE , IRON , PITCH , WHEAT , FLOUR, BREAD, HOP, BEAR,ARMOUR , SWORD, ARCHER, SPEAR
-    ,WOOD, OIL, HORSE, APPLE, CHEESE,MEAT;
+    STONE(TypeOfResource.INDUSTRY), IRON(TypeOfResource.INDUSTRY), PITCH(TypeOfResource.INDUSTRY), WHEAT(TypeOfResource.FOOD),
+    FLOUR(TypeOfResource.FOOD), BREAD(TypeOfResource.FOOD), HOP(TypeOfResource.FOOD), BEAR(TypeOfResource.FOOD),
+    ARMOUR(TypeOfResource.WEAPON), SWORD(TypeOfResource.WEAPON), ARCHER(TypeOfResource.WEAPON), SPEAR(TypeOfResource.WEAPON),
+    WOOD(TypeOfResource.INDUSTRY), OIL(TypeOfResource.INDUSTRY), HORSE(TypeOfResource.INDUSTRY), APPLE(TypeOfResource.FOOD),
+    CHEESE(TypeOfResource.FOOD), MEAT(TypeOfResource.FOOD);
+
+    private enum TypeOfResource {
+        INDUSTRY, WEAPON, FOOD
+    }
+
+    TypeOfResource typeOfResource;
+
+    Resource(TypeOfResource typeOfResource) {
+        this.typeOfResource = typeOfResource;
+    }
 }
