f6552e18776a3b3835b28c44f6586942d9c2923e
sepehr <ghiassepehr@gmail.com>
2023-04-25 03:36:05 +0330

    map : add blue area to type of tile

--- warSalib/src/main/java/model/map/type/Type.java
+++ warSalib/src/main/java/model/map/type/Type.java
@@ -6,7 +6,10 @@ public enum Type {
     ,SLATE(Area.BROWNAREA, true), STONE(Area.BROWNAREA, false),
     IRON_GROUND(Area.BROWNAREA, true), GRASS(Area.BROWNAREA, true),
     GRASSLAND(Area.BROWNAREA, true), DENSE_GRASSLAND(Area.BROWNAREA, true) ,
-    ;
+    OIL(Area.BLUEAREA, true), PLAIN(Area.BLUEAREA, true),
+    SHALLOW_WATER(Area.BLUEAREA, true), SEA(Area.BLUEAREA, false)
+    ,BEACH(Area.BLUEAREA, true), RIVER(Area.BLUEAREA, false),
+    BIG_POND(Area.BLUEAREA, false), SMALL_POND(Area.BLUEAREA, false);
 
     enum Area {
         BLUEAREA,BROWNAREA;
