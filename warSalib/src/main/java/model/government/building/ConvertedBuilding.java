7dd0ea0bd204f508a1fd62e6b4671c38041b9313
sepehr <ghiassepehr@gmail.com>
2023-04-28 16:54:46 +0330

    building : fix wall and tower and create caged and siegent tent and dog

--- warSalib/src/main/java/model/government/building/ConvertedBuilding.java
+++ warSalib/src/main/java/model/government/building/ConvertedBuilding.java
@@ -4,15 +4,18 @@ import model.government.Government;
 import model.government.resource.Resource;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ConvertedBuilding extends Building{
-    private int rate;
+    private int numberOfPrimitive;
+    private int NumberOfFinal;
     private Resource primitiveResource;
     private Resource finalResource;
 
-    public ConvertedBuilding(int x, int y, Government government, int hp, String type, String name, int rate, Resource primitiveResource, Resource finalResource) {
+    public ConvertedBuilding(int x, int y, Government government, int hp, String type, String name, int numberOfPrimitive, int NumberOfFinal, Resource primitiveResource, Resource finalResource) {
         super(x, y, government, hp, type, name);
-        this.rate = rate;
+        this.numberOfPrimitive = numberOfPrimitive;
+        this.NumberOfFinal = NumberOfFinal;
         this.primitiveResource = primitiveResource;
         this.finalResource = finalResource;
     }
@@ -22,7 +25,7 @@ public class ConvertedBuilding extends Building{
             HashMap<Resource, Integer> resource= new HashMap<>();
             resource.put(Resource.WOOD, 20);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding mill = new ConvertedBuilding(x, y, government, 300, "food processing building", name, 20, Resource.WHEAT, Resource.FLOUR);
+                ConvertedBuilding mill = new ConvertedBuilding(x, y, government, 300, "food processing building", name, 1,20, Resource.WHEAT, Resource.FLOUR);
                 return mill;
             }
         }
@@ -31,7 +34,7 @@ public class ConvertedBuilding extends Building{
             resource.put(Resource.WOOD, 20);
             resource.put(Resource.COIN, 100);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding armourer = new ConvertedBuilding(x, y, government, 600, "weapon", name, 3, Resource.IRON, Resource.ARMOUR);
+                ConvertedBuilding armourer = new ConvertedBuilding(x, y, government, 600, "weapon", name, 6,1, Resource.IRON, Resource.ARMOUR);
                 return armourer;
             }
         }
@@ -40,7 +43,7 @@ public class ConvertedBuilding extends Building{
             resource.put(Resource.WOOD, 20);
             resource.put(Resource.COIN, 100);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding blacksmith = new ConvertedBuilding(x, y, government, 700, "weapon", name, 3, Resource.IRON, Resource.SWORD);
+                ConvertedBuilding blacksmith = new ConvertedBuilding(x, y, government, 700, "weapon", name, 4, 1, Resource.IRON, Resource.SWORD);
                 return blacksmith;
             }
         }
@@ -49,7 +52,7 @@ public class ConvertedBuilding extends Building{
             resource.put(Resource.WOOD, 20);
             resource.put(Resource.COIN, 100);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding fletcher = new ConvertedBuilding(x, y, government, 600, "weapon", name, 5, Resource.IRON, Resource.ARCHER);
+                ConvertedBuilding fletcher = new ConvertedBuilding(x, y, government, 600, "weapon", name, 5,1, Resource.WOOD, Resource.ARCHER);
                 return fletcher;
             }
         }
@@ -58,7 +61,7 @@ public class ConvertedBuilding extends Building{
             resource.put(Resource.WOOD, 20);
             resource.put(Resource.COIN, 100);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding poleTurner = new ConvertedBuilding(x, y, government, 500, "weapon", name, 6, Resource.IRON, Resource.SPEAR);
+                ConvertedBuilding poleTurner = new ConvertedBuilding(x, y, government, 500, "weapon", name, 10,1, Resource.WOOD, Resource.SPEAR);
                 return poleTurner;
             }
         }
@@ -66,7 +69,7 @@ public class ConvertedBuilding extends Building{
             HashMap<Resource, Integer> resource= new HashMap<>();
             resource.put(Resource.WOOD, 10);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding bakery = new ConvertedBuilding(x, y, government, 200, "food processing building", name, 40, Resource.FLOUR, Resource.BREAD);
+                ConvertedBuilding bakery = new ConvertedBuilding(x, y, government, 200, "food processing building", name, 10,1, Resource.FLOUR, Resource.BREAD);
                 return bakery;
             }
         }
@@ -74,7 +77,7 @@ public class ConvertedBuilding extends Building{
             HashMap<Resource, Integer> resource= new HashMap<>();
             resource.put(Resource.WOOD, 10);
             if (government.hasEnoughResources(resource)) {
-                ConvertedBuilding brewery = new ConvertedBuilding(x, y, government, 350, "food processing building", name, 30, Resource.HOP, Resource.BEAR);
+                ConvertedBuilding brewery = new ConvertedBuilding(x, y, government, 350, "food processing building", name, 1,30, Resource.HOP, Resource.BEAR);
                 return brewery;
             }
         }
@@ -85,5 +88,11 @@ public class ConvertedBuilding extends Building{
         return 0;
     }
 
+    public int getNumberOfPrimitive() {
+        return numberOfPrimitive;
+    }
 
+    public int getNumberOfFinal() {
+        return NumberOfFinal;
+    }
 }
