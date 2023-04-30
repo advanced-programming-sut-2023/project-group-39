3704880b7b5e3f9e6f01454bde38f7ad17402652
sepehr <ghiassepehr@gmail.com>
2023-04-30 17:32:59 +0330

    main : added combat

--- warSalib/src/main/java/model/government/people/units/Combat.java
+++ warSalib/src/main/java/model/government/people/units/Combat.java
@@ -1,2 +1,33 @@
-package model.government.people.units;public class Combat {
+package model.government.people.units;
+
+import model.user.User;
+import model.wartool.wartoolenum;
+
+public class Combat extends Units{
+    private wartoolenum wartool;
+    public Combat(int xLocation, int yLocation, UnitsName unitsName, User ownerPerson) {
+        super(xLocation, yLocation, unitsName, ownerPerson);
+        if(unitsName.getName().equals("pikeman")){
+            this.wartool=wartoolenum.SPEAR;
+        }
+        else if(unitsName.getName().equals("swordsmen")){
+            this.wartool=null;
+        }
+        else if(unitsName.getName().equals("knight")){
+            this.hasHorse=true;
+        }
+        else if(unitsName.getName().equals("blackmonk")){
+            this.wartool=wartoolenum.STICK;
+        }
+        else if(unitsName.getName().equals("slaves")){
+            this.wartool=wartoolenum.TORCH;
+        }
+        else if(unitsName.getName().equals("arabianswordsmen")){
+            this.wartool=null;
+        }
+    }
+
+    public wartoolenum getWartool() {
+        return wartool;
+    }
 }
