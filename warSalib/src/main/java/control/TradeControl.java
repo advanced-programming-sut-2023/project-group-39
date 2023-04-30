3e0f18bee7e05d42b2e4ed6f97e4154ba9a8ccc4
Keyhan-Hodaei <118798460+Keyhan-Hodaei@users.noreply.github.com>
2023-04-30 17:56:27 +0430

    trade function: completed

--- warSalib/src/main/java/control/TradeControl.java
+++ warSalib/src/main/java/control/TradeControl.java
@@ -1,10 +1,27 @@
 package control;
 
+import model.Game;
+import model.government.Government;
+import model.government.resource.Resource;
+import view.TradeMenu;
 import view.enums.messages.TradeMenuMessage;
+
+import java.util.HashMap;
 
 public class TradeControl {
     public static TradeMenuMessage trade(String itemName, int amount, int price, String message) {
-        return null;
+        Government government = Game.getCurrentUser().getUserGovernment();
+
+        if (amount <= 0 || price < 0)
+            return TradeMenuMessage.INVALID_AMOUNT_OR_PRICE;
+
+        else if (!government.getResources().containsKey(itemName))
+            return TradeMenuMessage.ITEM_NOT_EXIST;
+
+        HashMap<Resource, Integer> tradeListValue = new HashMap<>();
+        tradeListValue.put(Resource.valueOf(itemName), amount);
+        TradeMenu.tradeList.put(government, tradeListValue);
+        return TradeMenuMessage.SUCCESS;
     }
 
     public static String showTradeList() {
