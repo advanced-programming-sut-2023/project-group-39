328a0567669c7ecc2cfcfcfb1394306ac30b6e1f
Keyhan-Hodaei <118798460+Keyhan-Hodaei@users.noreply.github.com>
2023-05-01 11:22:24 +0430

    deleted comments

--- warSalib/src/main/java/view/enums/messages/TradeMenuMessage.java
+++ warSalib/src/main/java/view/enums/messages/TradeMenuMessage.java
@@ -1,13 +1,13 @@
 package view.enums.messages;
 
 public enum TradeMenuMessage {
-    INVALID_AMOUNT_OR_PRICE,        //checked
-    ITEM_NOT_EXIST,                 //checked
-    EMPTY_TRADE_LIST,               //checked
-    ID_NOT_EXIST,                   //checked
-    SAME_ID_TO_CURRENT_USERS_ID,    //checked
-    RESOURCE_NOT_EXIST,             //checked
-    INSUFFICIENT_FUNDING,           //checked
+    INVALID_AMOUNT_OR_PRICE,
+    ITEM_NOT_EXIST,
+    EMPTY_TRADE_LIST,
+    ID_NOT_EXIST,
+    SAME_ID_TO_CURRENT_USERS_ID,
+    RESOURCE_NOT_EXIST,
+    INSUFFICIENT_FUNDING,
     EMPTY_TRADE_HISTORY,
     SUCCESS;
 }
