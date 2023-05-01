ed68f18b162d569c44ab48bb86bbd31d267a466f
Keyhan-Hodaei <118798460+Keyhan-Hodaei@users.noreply.github.com>
2023-05-01 11:19:44 +0430

    edited showTradeHistory and showTradeList functions

--- warSalib/src/main/java/control/TradeControl.java
+++ warSalib/src/main/java/control/TradeControl.java
@@ -34,14 +34,21 @@ public class TradeControl {
             return String.valueOf(TradeMenuMessage.EMPTY_TRADE_LIST);
 
         result.append("Trade List:");
-        for (Government government : TradeMenu.tradeList.keySet())
+        for (Government government : TradeMenu.tradeList.keySet()) {
             result.append("\nID: ")
                     .append(government.getUser().getUsername())
-                    .append("\t|\tResource: ")
-                    .append(TradeMenu.tradeList.get(government).keySet())
-                    .append("\t|\tAmount: ")
-                    .append(TradeMenu.tradeList.get(government).values());
+                    .append("\t|\tResource: ");
+            Set<Resource> set = TradeMenu.tradeList.get(government).keySet();
+            Iterator<Resource> iterator = set.iterator();
+            Resource resource = null;
+            if (iterator.hasNext()) resource = iterator.next();
 
+            int amount = TradeMenu.tradeList.get(government).get(resource);
+
+            result.append(resource)
+                    .append("\t|\tAmount: ")
+                    .append(amount);
+        }
         return result.toString();
     }
 
@@ -57,11 +64,13 @@ public class TradeControl {
                     break;
                 }
 
-            Set<Resource> set= TradeMenu.tradeList.get(user.getUserGovernment()).keySet();
+            Set<Resource> set = TradeMenu.tradeList.get(user.getUserGovernment()).keySet();
             Iterator<Resource> iterator = set.iterator();
             Resource resource = null;
             if (iterator.hasNext()) resource = iterator.next();
+
             int amount = TradeMenu.tradeList.get(user.getUserGovernment()).get(resource);
+
             if (!currentUser.getUserGovernment().getResources().containsKey(resource))
                 return TradeMenuMessage.RESOURCE_NOT_EXIST;
             else if (currentUser.getUserGovernment().getResources().get(resource) < amount)
@@ -77,7 +86,28 @@ public class TradeControl {
         return TradeMenuMessage.ID_NOT_EXIST;
     }
     public static String showTradeHistory() {
-        return null;
+        StringBuilder result = new StringBuilder();
+        Government government = Game.getCurrentUser().getUserGovernment();
+        if (government.getTradeHistory().isEmpty())
+            return String.valueOf(TradeMenuMessage.EMPTY_TRADE_HISTORY);
+
+        result.append("Trade History:");
+        for (User user : government.getTradeHistory().keySet()) {
+            result.append("\nUser: ")
+                    .append(user.getUsername())
+                    .append("\tItem: ");
+            Set<Resource> set = government.getTradeHistory().get(user).keySet();
+            Iterator<Resource> iterator = set.iterator();
+            Resource resource = null;
+            if (iterator.hasNext()) resource = iterator.next();
+
+            int amount = government.getTradeHistory().get(user).get(resource);
+
+            result.append(resource)
+                    .append("\tAmount: ")
+                    .append(amount);
+        }
+        return result.toString();
     }
 
     private static boolean validIdForAccept (String id) {
