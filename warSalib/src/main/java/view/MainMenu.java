e6f53ed09fd059d93355980c3ab971fc29286043
ardalan <ardaldehghan0@gmail.com>
2023-05-11 23:20:32 +0430

    main : game start

--- warSalib/src/main/java/view/MainMenu.java
+++ warSalib/src/main/java/view/MainMenu.java
@@ -1,5 +1,8 @@
 package view;
 
+import model.Game;
+import model.map.GameMap;
+import model.user.User;
 import view.enums.commands.MainMenuCommands;
 
 import java.util.Scanner;
@@ -25,9 +28,50 @@ public class MainMenu {
                 }
             }
             else if ((matcher = MainMenuCommands.getMatcher(input, MainMenuCommands.START_GAME)) != null) {
-
+                letStartGame();
+                EnvironmentMenu.run(scanner);
             } else System.out.println("invalid command!");
 
         }
+    }
+    private static void letStartGame(){
+        Game.getPlayersInGame();
+        int counter=0;
+        Game.getPlayersInGame().add(Game.getCurrentUser());
+        counter++;
+        Scanner scanner=new Scanner(System.in);
+        while (true){
+            String add=scanner.nextLine();
+            Matcher matcher;
+            if((matcher = MainMenuCommands.getMatcher(add, MainMenuCommands.ADD_PLAYER)) != null){
+                String username=matcher.group("username");
+                if(getUserByName(username)!=null) {
+                    if(counter==8){
+                        System.out.println("you have maximum of players in your game");
+                    }
+                    Game.getPlayersInGame().add(getUserByName(username));
+                    counter++;
+                }
+                else
+                    System.out.println("we dont have this user");
+
+            } else if (add.matches("^\\s*make\\s+map\\s*$")){
+                GameMap gameMap = new GameMap();
+                Game.setMapInGame(gameMap);
+                break;
+            }
+            else {
+                System.out.println("invalid command");
+
+            }
+        }
+    }
+    private static User getUserByName(String username){
+        for (User user:Game.getPlayers()){
+            if(user.getUsername().equals(username))
+                return user;
+
+        }
+        return null;
     }
 }
