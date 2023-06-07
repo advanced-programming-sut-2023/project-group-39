5a604c61da7834231a64c62653443ba9be8f6632
ardalan <ardaldehghan0@gmail.com>
2023-06-07 17:40:45 +0330

    git commit -m "before profile"

--- warSalib/src/main/java/view/MapMenu.java
+++ warSalib/src/main/java/view/MapMenu.java
@@ -39,36 +39,76 @@ public class MapMenu extends Application {
     public Label population;
     private int tileSize = 50;
 
-    private Tile [][] tiles;
+    private Tile[][] tiles;
 
-    private ArrayList<Tile> selectedTile = new ArrayList<>();
+    private static ArrayList<Tile> selectedTile = new ArrayList<>();
+
+    private static Popup informationPopup = new Popup();
+
+    private static Stage mapStage;
+
     @Override
     public void start(Stage stage) throws Exception {
+        mapStage = stage;
         GridPane gridPane = createTileMap();
         javafx.scene.control.ScrollPane scrollPane = new ScrollPane(gridPane);
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
-        BorderPane borderPane = FXMLLoader.load(ProfileMenu.class.getResource("/fxml/mapMenu.fxml"));;
+        BorderPane borderPane = FXMLLoader.load(ProfileMenu.class.getResource("/fxml/mapMenu.fxml"));
+        ;
         borderPane.setCenter(scrollPane);
         Scene scene = new Scene(borderPane);
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while(true) {
+                while (true) {
                     scrollPane.requestFocus();
                     scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
                         @Override
                         public void handle(KeyEvent keyEvent) {
-                            if (keyEvent.getCode() == KeyCode.EQUALS){
+                            if (keyEvent.getCode() == KeyCode.EQUALS) {
                                 System.out.println("zoom In");
                                 zoomIn();
-                            }
-                            else if(keyEvent.getCode() == KeyCode.MINUS){
+                            } else if (keyEvent.getCode() == KeyCode.MINUS) {
                                 System.out.println("zoom out");
                                 zoomOut();
+                            } else if (keyEvent.getCode() == KeyCode.A) {
+                                selectLocationForMove(selectedTile);
+
                             }
                         }
                     });
+                }
+            }
+
+            private void selectLocationForMove(ArrayList<Tile> selectedTile) {
+                ArrayList<Units> playerUnit = new ArrayList<>();
+                if (selectedTile == null) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("please select a tile");
+                    alert.showAndWait();
+                } else if (selectedTile.size() > 1) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("please choose just one tile");
+                    alert.showAndWait();
+
+                } else {
+                    for (People units : selectedTile.get(0).getPeopleOnTile()) {
+                        if (units instanceof Units && units.getOwnerPerson().equals(Game.getTurnedUserForGame())) {
+                            playerUnit.add((Units) units);
+
+                        }
+                    }
+                    if (playerUnit.size() == 0) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("you dont have any unit on this tile");
+                        alert.showAndWait();
+
+                    } else {
+
+                        Popup chooseUnits=new Popup();
+
+                    }
                 }
             }
         }).start();
@@ -79,6 +119,10 @@ public class MapMenu extends Application {
 
     @FXML
     public void initialize() {
+        popularity.setText(String.valueOf(Game.getTurnedUserForGame().getUserGovernment().getPopularity()));
+        wealth.setText(String.valueOf(Game.getTurnedUserForGame().getUserGovernment().getWealth()));
+        population.setText(String.valueOf(Game.getTurnedUserForGame().getUserGovernment().getPopulation() + "/"
+                + Game.getTurnedUserForGame().getUserGovernment().getPopulationCapacity()));
 //         popularity.setText(String.valueOf(Game.getTurnedUserForGame().getUserGovernment().getPopularity()));
 //         wealth.setText(String.valueOf(Game.getTurnedUserForGame().getUserGovernment().getWealth()));
 //         population.setText(String.valueOf(Game.getTurnedUserForGame().getUserGovernment().getPopulation() + "/"
@@ -97,11 +141,11 @@ public class MapMenu extends Application {
     }
 
     private void zoomOut() {
-        if (tileSize > 30 && tiles!= null) {
+        if (tileSize > 30 && tiles != null) {
             tileSize -= 5;
             Game.setTileSize(tileSize);
-            for (int j = 0 ; j < 100; j++){
-                for (int i = 0; i< 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                for (int i = 0; i < 100; i++) {
                     tiles[j][i].setMinWidth(tileSize);
                     tiles[j][i].setMinHeight(tileSize);
                 }
@@ -114,7 +158,7 @@ public class MapMenu extends Application {
             tileSize += 5;
             Game.setTileSize(tileSize);
             for (int j = 0; j < 100; j++) {
-                for (int i = 0; i < 100; i++){
+                for (int i = 0; i < 100; i++) {
                     tiles[j][i].setMinWidth(tileSize);
                     tiles[j][i].setMinHeight(tileSize);
                 }
@@ -127,17 +171,20 @@ public class MapMenu extends Application {
         Game.setMapInGame(gameMap);
         GridPane gridPane = new GridPane();
         tiles = Game.getMapInGame().getMap();
-        for (int i = 0 ; i < 100 ; i++) {
-            for (int j = 0; j< 100; j++) {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
                 Tile tile = tiles[i][j];
                 tile.setMinWidth(tileSize);
                 tile.setMinHeight(tileSize);
-                tile.setOnMouseEntered(this :: handleMouseEntered);
-                tile.setOnMouseExited(this :: handleMouseExited);
-               tile.setOnMouseClicked(event -> clickedAtBottom(tile));
+                tile.setOnMouseEntered(this::handleMouseEntered);
+                tile.setOnMouseExited(this::handleMouseExited);
+                tile.setOnMouseClicked(event -> clickedAtBottom(tile));
                 setTileTooltip(tiles[i][j], i, j);
                 gridPane.add(tiles[i][j], i, j);
             }
+        }
+        for (Government government : Game.getGovernments()) {
+            MainMenu.createInitialPeople(government, 30);
         }
         return gridPane;
     }
@@ -147,29 +194,69 @@ public class MapMenu extends Application {
         if (!selectedTile.contains(tile)) {
             selectedTile.add(tile);
             tile.setOpacity(0.2);
-        }
-        else {
+        } else {
             selectedTile.remove(tile);
             tile.setOpacity(1);
         }
     }
 
     private void setTileTooltip(Tile tile, int i, int j) {
-        String details = MapControl.showDetails(j,i);
-        Tooltip tooltip = new Tooltip(details);
-        Tooltip.install(tile,tooltip);
+        String details = MapControl.showDetails(j, i);
+        Game.getMapInGame().getMap()[j][i].getTooltip().setText(details);
+        Tooltip.install(tile, Game.getMapInGame().getMap()[j][i].getTooltip());
     }
 
     private void handleMouseExited(MouseEvent mouseEvent) {
         Tile tile = (Tile) mouseEvent.getSource();
-        if(!selectedTile.contains(tile))
+        if (!selectedTile.contains(tile))
             tile.setOpacity(1);
+        hideUnitsInformation(tile);
+    }
+
+    private void hideUnitsInformation(Tile tile) {
+        if (informationPopup.isShowing()) {
+            informationPopup.hide();
+
+        }
+    }
+
+    private void showUnitsDetail(Tile tile) {
+        VBox vBox = new VBox();
+        vBox.setStyle("-fx-background-color: #DCDC91B6");
+        vBox.setSpacing(15);
+        for (People units : tile.getPeopleOnTile()) {
+            if (units instanceof Units) {
+                HBox hBox = new HBox();
+                hBox.setSpacing(10);
+                Label empty = new Label("");
+                Label unitName = new Label(((Units) units).getUnitsName().getName());
+                Label unitOwner = new Label(units.getOwnerPerson().getUsername());
+                String power = String.valueOf(((Units) units).getUnitsName().getAttackingPower());
+                Label unitPower = new Label(power);
+                String health = String.valueOf(((Units) units).getHitPoint());
+                Label unitHealth = new Label(health);
+                String Mode = String.valueOf(((Units) units).getState());
+                Label unitMode = new Label(Mode);
+                hBox.getChildren().addAll(empty, unitName, unitOwner, unitPower, unitHealth, unitMode);
+                vBox.getChildren().add(hBox);
+
+            }
+
+        }
+        if (informationPopup.getContent() != null) {
+            informationPopup.getContent().clear();
+        }
+        if (tile.getPeopleOnTile().size() != 0) {
+            informationPopup.getContent().add(vBox);
+            informationPopup.show(mapStage);
+        }
     }
 
     private void handleMouseEntered(MouseEvent mouseEvent) {
         Tile tile = (Tile) mouseEvent.getSource();
         if (!selectedTile.contains(tile))
             tile.setOpacity(0.5);
+        showUnitsDetail(tile);
 
     }
 
@@ -191,7 +278,7 @@ public class MapMenu extends Application {
 
     private Tile[][] showMap(int x, int y) {
         Tile[][] result;
-        if ((result = MapControl.showMap(x, y))==null)
+        if ((result = MapControl.showMap(x, y)) == null)
             return result;
         else
             return null;
@@ -227,6 +314,7 @@ public class MapMenu extends Application {
         System.out.println(MapControl.moveMap(up, down, right, left));
 
     }
+
     public int getTileSize() {
         return tileSize;
     }
