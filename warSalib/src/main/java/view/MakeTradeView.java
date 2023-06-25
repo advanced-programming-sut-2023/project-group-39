ddad6d39471fb7a8165d6d17b781d45ec1a0daba
Sayyed Keyhan Hodaei <118798460+Keyhan-Hodaei@users.noreply.github.com>
2023-06-25 14:50:30 +0330

    makeTrade is complete but not tested

--- warSalib/src/main/java/view/MakeTradeView.java
+++ warSalib/src/main/java/view/MakeTradeView.java
@@ -1,11 +1,133 @@
 package view;
 
+import control.TradeControl;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.Game;
 
 public class MakeTradeView extends Application {
+    private int resourceMax = 0;
+    private final ToggleGroup toggleGroup = new ToggleGroup();
+    private String selectedResource;
+
+    @javafx.fxml.FXML
+    private TextField priceField;
+    @javafx.fxml.FXML
+    private Button decreaseButton;
+    @javafx.fxml.FXML
+    private Label resourceAmountLabel;
+    @javafx.fxml.FXML
+    private Button increaseButton;
+    @javafx.fxml.FXML
+    private TextArea messageTextArea;
+    @javafx.fxml.FXML
+    private RadioButton donateRadio;
+    @javafx.fxml.FXML
+    private RadioButton requestRadio;
+    @javafx.fxml.FXML
+    private Button sendButton;
+    @javafx.fxml.FXML
+    private GridPane resourceGrid;
+    @javafx.fxml.FXML
+    private ChoiceBox userSelectDropdown;
+
     @Override
     public void start(Stage stage) throws Exception {
+        Pane pane = FXMLLoader.load(getClass().getResource("/fxml/makeTrade.fxml"));
+        decreaseButton = (Button) pane.lookup("#decreaseButton");
+        resourceAmountLabel = (Label) pane.lookup("#resourceAmountLabel");
+        increaseButton = (Button) pane.lookup("#increaseButton");
+        messageTextArea = (TextArea) pane.lookup("#messageTextArea");
+        donateRadio = (RadioButton) pane.lookup("#donateRadio");
+        requestRadio = (RadioButton) pane.lookup("#requestRadio");
+        sendButton = (Button) pane.lookup("#sendButton");
+        resourceGrid = (GridPane) pane.lookup("#resourceGrid");
+        priceField = (TextField) pane.lookup("#priceField");
+        userSelectDropdown = (ChoiceBox) pane.lookup("#userSelectDropdown");
+        userSelectDropdown.setSelectionModel(new SingleSelectionModel<String>() {
+            @Override
+            protected String getModelItem(int i) {
+                return Game.getPlayers().get(i).getNickname();
+            }
 
+            @Override
+            protected int getItemCount() {
+                return Game.getPlayers().size();
+            }
+        });
+        userSelectDropdown.getSelectionModel().selectFirst();
+        decreaseButton.setOnAction(event -> resourceAmountLabel.setText(String.valueOf(Math.max(Integer.parseInt(resourceAmountLabel.getText()) - 1, 0))));
+        increaseButton.setOnAction(event -> resourceAmountLabel.setText(String.valueOf(Math.min(Integer.parseInt(resourceAmountLabel.getText()) + 1, resourceMax))));
+
+        donateRadio.setToggleGroup(toggleGroup);
+        requestRadio.setToggleGroup(toggleGroup);
+
+        String[] resources = {"wood", "archery", "armour", "pitch", "bread", "coin", "flour", "food", "hammer", "hop", "oil", "spear", "steel", "stone", "sword", "wheat"};
+        for (int i = 0; i < resources.length; i++) {
+            ImageView temp = new ImageView(new Image(MakeTradeView.class.getResource("/images/resource/" + resources[i] + ".png").toExternalForm()));
+            temp.setOnMouseClicked(event -> {
+                resourceAmountLabel.setText("0");
+                resourceGrid.getChildren().forEach(node -> {
+                    ((ImageView) node).setFitHeight(32);
+                    ((ImageView) node).setFitWidth(32);
+                    node.setMouseTransparent(false);
+                });
+                temp.setFitHeight(64);
+                temp.setFitWidth(64);
+                temp.setMouseTransparent(true);
+                selectedResource = resources[resourceGrid.getChildren().indexOf(temp)];
+                Game.getTurnedUserForGame().getUserGovernment().getResources().forEach((resource, amount) -> {
+                    if (resource.equals(selectedResource)) {
+                        resourceMax = amount;
+                    }
+                });
+            });
+            temp.setFitHeight(32);
+            temp.setFitWidth(32);
+            temp.setPickOnBounds(true);
+            resourceGrid.add(temp, i % 6, i / 6);
+        }
+
+        sendButton.setOnAction(event -> {
+            if (toggleGroup.getSelectedToggle() == null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("No trade type selected");
+                alert.setContentText("Please select a trade type");
+                alert.showAndWait();
+            } else if (selectedResource == null) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("No resource selected");
+                alert.setContentText("Please select a resource");
+                alert.showAndWait();
+            } else if (Integer.parseInt(resourceAmountLabel.getText()) == 0) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("No resource amount selected");
+                alert.setContentText("Please select a resource amount");
+                alert.showAndWait();
+            } else {
+                TradeControl.trade(selectedResource, Integer.parseInt(resourceAmountLabel.getText()), Integer.parseInt(priceField.getText()) , messageTextArea.getText());
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Success");
+                alert.setHeaderText("Trade request sent");
+                alert.setContentText("Your trade request has been sent");
+                alert.showAndWait();
+                stage.close();
+            }
+        });
+        stage.setTitle("Make a Trade");
+        stage.setScene(new Scene(pane));
     }
 }
--- warSalib/src/main/java/view/TradeView.java
+++ warSalib/src/main/java/view/TradeView.java
@@ -2,11 +2,16 @@ package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import model.user.User;
+
+import java.awt.*;
 
 public class TradeView extends Application {
     public static Stage stage;
@@ -15,19 +20,46 @@ public class TradeView extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        BorderPane pane = FXMLLoader.load(StartGame.class.getResource("/fxml/TradeView.fxml"));
+        BorderPane pane = new BorderPane();
         TradeView.stage = stage;
         stage.setTitle("Trade Menu");
+        stage.setHeight(100);
+        stage.setWidth(300);
+        stage.setResizable(false);
+
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+        //add 2 buttons for suggestTrade and viewOffers
+
+        pane.setCenter(hbox);
+        Button suggestTrade = new Button("Make a new Trade");
+        suggestTrade.setOnMouseClicked(this::makeNewTrade);
+        Button viewOffers = new Button("View Trade list");
+        viewOffers.setOnMouseClicked(this::viewTradeList);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.getChildren().addAll(suggestTrade, viewOffers);
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
     }
 
-    public void viewTradeList(MouseEvent mouseEvent) throws Exception {
-        new TradeSuggestsView().start(TradeView.stage);
+    public void viewTradeList(MouseEvent mouseEvent) {
+        try {
+            new TradeSuggestsView().start(TradeView.stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void makeNewTrade(MouseEvent mouseEvent) throws Exception {
-        new MakeTradeView().start(TradeView.stage);
+    public void makeNewTrade(MouseEvent mouseEvent) {
+        try {
+            stage.setHeight(500);
+            stage.setWidth(750);
+            stage.setX(Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2 - 375);
+            stage.setY(Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2 - 250);
+            new MakeTradeView().start(TradeView.stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
