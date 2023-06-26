d0b50391ccc325d3b9363418f41f4efa96effe2b
ardalan <ardaldehghan0@gmail.com>
2023-06-26 23:43:52 +0330

    score board

--- warSalib/src/main/java/view/scoreBoardView.java
+++ warSalib/src/main/java/view/scoreBoardView.java
@@ -2,76 +2,135 @@ package view;
 
 import com.google.gson.Gson;
 import control.LoginSignupControl;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import java.awt.ScrollPane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import model.Game;
 import model.user.User;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.*;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.awt.ScrollPane;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class scoreBoardView  extends Application {
+public class scoreBoardView extends JFrame {
+    private JScrollPane scrollPane;
+    private JPanel panel;
+    private int dataCount = Game.getPlayers().size();
+    private int visibleCount = 10;
+    private int loadedCount = 0;
 
-    public static GridPane scoreBoardPane;
-
-    public static Stage scoreBoardStage;
-    @Override
-    public void start(Stage stage) throws Exception {
-            GridPane pane = FXMLLoader.load(StartGame.class.getResource("/fxml/ScoreBoard.fxml"));
-            scoreBoardPane = pane;
-          // javafx.scene.control.ScrollPane scrollPane=new ScrollPane(pane);
-            Scene scene = new Scene(pane,840,720);
-            scoreBoardStage = stage;
-            stage.setTitle("Score Board");
-            stage.setScene(scene);
-            stage.show();
-       LoginSignupControl.sort(Game.getPlayers());
-       String ranking= String.valueOf(Game.getPlayers().indexOf(Game.getCurrentUser())+1);
-       Text rank=new Text(ranking);
-       String scoring= String.valueOf(Game.getCurrentUser().getScore());
-       Text score=new Text(scoring);
-       rank.setLayoutX(60);
-       rank.setLayoutY(30);
-       score.setLayoutX(60);
-       score.setLayoutY(50);
-       scoreBoardPane.getChildren().addAll(rank,score);
-            showRankings(Game.getPlayers());
+    public scoreBoardView() {
+        LoginSignupControl.sort(Game.getPlayers());
+        setTitle("Score board");
+        setSize(600, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        panel.setBackground(Color.green);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        scrollPane = new JScrollPane(panel);
+        JButton back = new JButton("back to profile menu");
+        //  panel.add(back);
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) getContentPane().getLayout();
+                cardLayout.show(getContentPane(), StartGame.class.getResource("/fxml/ProfileView.fxml").toExternalForm());
+            }
+        });
+        scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {
+            @Override
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+                JScrollBar scrollBar = (JScrollBar) e.getSource();
+                int extent = scrollBar.getModel().getExtent();
+                int maximum = scrollBar.getModel().getMaximum();
+                int value = scrollBar.getValue();
+                if (value + extent == maximum) {
+                    try {
+                        loadMoreData();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+        add(scrollPane);
+        setVisible(true);
     }
 
-    private void showRankings(ArrayList<User> sortedUsers) {
-        VBox ranking=new VBox();
-        ranking.setAlignment(Pos.CENTER);
-        for (int i=0;i<Game.getPlayers().size();i++){
-            String rank= String.valueOf(i+1);
-            Text username=new Text(Game.getPlayers().get(i).getUsername());
-            Text userRank=new Text(rank);
-            String scoring= String.valueOf(Game.getPlayers().get(i).getScore());
-            Text userScore=new Text(scoring);
-            HBox hBox=new HBox((Node) userRank, (Node) username, (Node) userScore);
-            ranking.getChildren().add(hBox);
-        }
-        ScrollPane scrollPane=new ScrollPane();
-        try {
-            FileWriter fileWriter=new FileWriter("users.json");
+    private void loadMoreData() throws IOException {
+        for (int i = loadedCount; i < loadedCount + visibleCount && i < dataCount; i++) {
+            JLabel label = new JLabel("Rank:  " + (i + 1) + "  Username: " + Game.getPlayers().get(i).getUsername() + " Points: " + Game.getPlayers().get(i).getScore());
+            EmptyBorder border = new EmptyBorder(10, 20, 15, 20);
+            label.setBorder(border);
+            MouseListener mouseListener = new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    try {
+                        setImageForUser(label);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            };
+            label.addMouseListener(mouseListener);
+            if (Game.getPlayers().get(i).getChooseImageAddress() != null) {
+                ImageIcon imageIcon = new ImageIcon(Game.getPlayers().get(i).getChooseImageAddress()); // load the image to a imageIcon
+                Image image = imageIcon.getImage(); // transform it
+                Image newimg = image.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+                imageIcon = new ImageIcon(newimg);
+                label.setIcon(imageIcon);
+            } else if (Game.getPlayers().get(i).getAvatarImageAddress() != null) {
+                ImageIcon imageIcon = new ImageIcon(StartGame.class.getResource(Game.getPlayers().get(i).getAvatarImageAddress()));
+                Image image = imageIcon.getImage();
+                Image newImg = image.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+                imageIcon = new ImageIcon(newImg);
+                label.setIcon(imageIcon);
+            }
+            FileWriter fileWriter = new FileWriter("users.json");
             fileWriter.write(new Gson().toJson(view.adam.adams));
             fileWriter.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            panel.add(label);
+            loadedCount += visibleCount;
+            panel.revalidate();
+            panel.repaint();
         }
-        //   scoreBoardPane.getChildren().add(scrollPane);
+    }
+
+    private void setImageForUser(JLabel label) throws IOException {
+        String regex = "^Rank:\\s+(?<number>[\\d]+)";
+        Matcher matcher = Pattern.compile(regex).matcher(label.getText());
+        if (matcher.find()) {
+            int number = Integer.parseInt(matcher.group("number"));
+            User user = Game.getPlayers().get(number - 1);
+            if (user.getChooseImageAddress() != null) {
+                Game.getCurrentUser().setChooseImageAddress(user.getChooseImageAddress());
+                for (adam adam : adam.adams) {
+                    if (adam.getUsername().equals(Game.getCurrentUser().getUsername())) {
+                        adam.chooseImageAddress=user.getChooseImageAddress();
+
+                    }
+                }
+            } else if (user.getAvatarImageAddress() != null) {
+                Game.getCurrentUser().setAvatarImageAddress(user.getAvatarImageAddress());
+                for (adam adam : adam.adams) {
+                    if (adam.getUsername().equals(Game.getCurrentUser().getUsername())) {
+                        adam.avatarImageAddress=user.getAvatarImageAddress();
+
+                    }
+                }
+
+            }
+            FileWriter fileWriter = new FileWriter("users.json");
+            fileWriter.write(new Gson().toJson(view.adam.adams));
+            fileWriter.close();
+        }
+    }
+
+
+    public void run() {
+        new scoreBoardView();
     }
 }
