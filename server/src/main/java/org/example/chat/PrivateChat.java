34a3f60fce6365c36acda4d5e63c9ee505f63fe4
sepehr <ghiassepehr@gmail.com>
2023-06-30 18:31:45 +0330

    server : completed

--- server/src/main/java/org/example/chat/PrivateChat.java
+++ server/src/main/java/org/example/chat/PrivateChat.java
@@ -1,4 +1,11 @@
 package org.example.chat;
 
+import org.example.User;
+
 public class PrivateChat extends Chat{
+    @Override
+    public synchronized void addPeople(User user) {
+        if (super.groupId.size() == 1)
+            super.addPeople(user);
+    }
 }
