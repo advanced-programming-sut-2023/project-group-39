91730daef97a0e8ccb01acbb166cd3e98dddf14c
ardalan <ardaldehghan0@gmail.com>
2023-06-30 11:46:12 +0330

    change in json

--- server/src/main/java/org/example/Database.java
+++ server/src/main/java/org/example/Database.java
@@ -7,16 +7,71 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Database {
     private static ArrayList<User> users = new ArrayList<>();
     private static User loggedInUser;
+
     public static void initUsers() throws IOException {
-        String json=null;
-        json=new String(Files.readAllBytes(Paths.get("users.json")));
-        ArrayList<User> data=new Gson().fromJson(json,new TypeToken<ArrayList<User>>(){}.getType());
-        users = data;
-        users.get(0).getNickname();
+        String json = null;
+        String usernameRegex = "\\\"username\\\":\\\"(?<username>[^\\\"]+)\\\"";
+        String passwordRegex = "\\\"password\\\":\\\"(?<password>[^\\\"]+)\\\"";
+        String emailRegex = "\\\"email\\\":\\\"(?<email>[^\\\"]+)\\\"";
+        String secAnswerRegex = "\\\"securityQuestionAnswer\\\":\\\"(?<senAns>[^\\\"]+)\\\"";
+        String sloganRegex = "\\\"slogan\\\":\\\"(?<slogan>[^\\\"]+)\\\"";
+        String scoreRegex = "\\\"score\\\":\\\"(?<score>[^\\\"]+)\\\"";
+        String chooseImageRegex = "\\\"chooseImageAddress\\\":\\\"(?<choose>[^\\\"]+)\\\"";
+        String nicknameRegex = "\\\"nickname\\\":\\\"(?<nickname>[^\\\"]+)\\\"";
+        // add avatar image address;
+        json = new String(Files.readAllBytes(Paths.get("users.json")));
+        String[] datas = json.split("\\}\\,");
+        for (int j = 0; j < datas.length; j++) {
+            String username = null;
+            String password = null;
+            String email = null;
+            String choose = null;
+            String score = null;
+            String secAnswer = null;
+            String slogan = null;
+            String nickname = null;
+            Matcher usernameMatcher = Pattern.compile(usernameRegex).matcher(datas[j]);
+            Matcher emailMatcher = Pattern.compile(emailRegex).matcher(datas[j]);
+            Matcher passwordMatcher = Pattern.compile(passwordRegex).matcher(datas[j]);
+            Matcher sloganMatcher = Pattern.compile(sloganRegex).matcher(datas[j]);
+            Matcher scoreMatcher = Pattern.compile(scoreRegex).matcher(datas[j]);
+            Matcher chooseMatcher = Pattern.compile(chooseImageRegex).matcher(datas[j]);
+            Matcher secAnswerMatcher = Pattern.compile(secAnswerRegex).matcher(datas[j]);
+            Matcher nicknameMatcher = Pattern.compile(nicknameRegex).matcher(datas[j]);
+            if (usernameMatcher.find()) {
+                username = usernameMatcher.group("username");
+            }
+            if (passwordMatcher.find()) {
+                password = passwordMatcher.group("password");
+            }
+            if (emailMatcher.find()) {
+                email = emailMatcher.group("email");
+            }
+            if (sloganMatcher.find()) {
+                slogan = sloganMatcher.group("slogan");
+            }
+            if (secAnswerMatcher.find()) {
+                secAnswer = secAnswerMatcher.group("senAns");
+            }
+            if (scoreMatcher.find()) {
+                score = scoreMatcher.group("score");
+            }
+            if (chooseMatcher.find()) {
+                choose = chooseMatcher.group("choose");
+            }
+            if (nicknameMatcher.find()) {
+                nickname = nicknameMatcher.group("nickname");
+            }
+            User user = new User(username, password, email, nickname, slogan, secAnswer);
+            users.add(user);
+
+        }
     }
 
     public static ArrayList<User> getUsers() {
@@ -30,19 +85,21 @@ public class Database {
     public static void setLoggedInUser(User loggedInUser) {
         Database.loggedInUser = loggedInUser;
     }
+
     public static Message hasUser(String username, String password) {
         //User user1= new User("ardalan","Ardal123!","ardali@gmail.com", "ardali","","khar");
         //users.add(user1);
         for (User user : users) {
             if (user.getUsername().equals(username)) {
             }
-                if (user.getPassword().equals(password))
-                    return Message.SUCCESS;
-                else return Message.WRONG_PASSWORD;
-            }
+            if (user.getPassword().equals(password))
+                return Message.SUCCESS;
+            else return Message.WRONG_PASSWORD;
+        }
 
         return Message.WRONG_USERNAME;
     }
+
     public static String hasCorrectSecurityQuestion(String username, String securityAnswer) {
         for (User user : users) {
             if (user.getUsername().equals(username)) {
@@ -55,6 +112,7 @@ public class Database {
         }
         return "invalidUsername";
     }
+
     public static User getUserByName(String username, String password) {
         for (User user : users) {
             if (user.getPassword().equals(password) && user.getUsername().equals(username))
